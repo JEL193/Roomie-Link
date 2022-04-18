662b6d1c1068ff704a5c6b3285e7d51d1dd21811
Jacob Lopez <Jel193@txstate.edu>
2022-04-17 19:30:42 -0500

    fixed dependncy issue tinder card

--- roomie-link/src/App.js
+++ roomie-link/src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import Header from './Header';
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-
-      
+    <div className="App"> 
         {/*header*/}
-        <Header/>
+        <Header />
 
-        {/*tinder card*/}
-
-        {/*buttons below tinder cards*/}
-               
-        {/*Chat screen*/}
-        {/* Individual at screen*/}
-
-
-      
 
     </div>
   );
